e833c7b22ee70e903095418c1989853b2488cd27
Hao Xu <xuh@cs.unc.edu>
2020-01-06 18:13:28 -0500

    update

--- Dockerfile
+++ Dockerfile
@@ -8,6 +8,7 @@ RUN pip3 install --no-cache-dir flask pymongo gunicorn[gevent]==19.9.0 connexion
 
 COPY api/openAPI3 /usr/src/app/api
 COPY pdsdpimockfhir /usr/src/app/pdsdpimockfhir
+COPY tx-utils/src /usr/src/app
 
 EXPOSE 8080
 
--- api/openAPI3/server.py
+++ api/openAPI3/server.py
@@ -1,8 +1,14 @@
 import connexion
 from flask_cors import CORS
+from tx.connexion.utils import ReverseProxied
 
 def create_app():
     app = connexion.FlaskApp(__name__, specification_dir='openapi/')
     app.add_api('my_api.yaml')
     CORS(app.app)
+    flask_app = app.app
+    proxied = ReverseProxied(
+        flask_app.wsgi_app
+    )
+    flask_app.wsgi_app = proxied
     return app
