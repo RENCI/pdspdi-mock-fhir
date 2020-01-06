f97a1e4985bdc46712ed8edcad208cb9dddeb461
Hao Xu <xuh@cs.unc.edu>
2020-01-06 17:33:28 -0500

    update

--- api/openAPI3/server.py
+++ api/openAPI3/server.py
@@ -1,11 +1,8 @@
-import os
 import connexion
 from flask_cors import CORS
 
-print(os.environ)
-
 def create_app():
     app = connexion.FlaskApp(__name__, specification_dir='openapi/')
-    app.add_api('my_api.yaml', arguments={"server_url": os.environ["SERVER_URL"]})
+    app.add_api('my_api.yaml')
     CORS(app.app)
     return app
