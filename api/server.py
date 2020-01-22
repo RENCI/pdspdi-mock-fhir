de14b8a66f88ee8ab4c015447a17cace2afdbd8c
xu-hao <xuh@cs.unc.edu>
2020-01-22 13:53:56 -0500

    add env var API_READONLY for  read only api

--- api/server.py
+++ api/server.py
@@ -1,10 +1,15 @@
+import os
 import connexion
 from flask_cors import CORS
 from tx.connexion.utils import ReverseProxied
 
+api_readonly = os.environ.get("API_READONLY", "")
 def create_app():
     app = connexion.FlaskApp(__name__, specification_dir='openapi/')
-    app.add_api('my_api.yaml')
+    if api_readonly == "1":
+        app.add_api('api_readonly.yaml')
+    else:
+        app.add_api('my_api.yaml')
     CORS(app.app)
     flask_app = app.app
     proxied = ReverseProxied(
