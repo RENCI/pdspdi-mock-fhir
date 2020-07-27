ccc9591bc093e3f5b95f6b4eaeede31e4dc46b8c
Hao Xu <xuh@cs.unc.edu>
2020-07-27 15:34:44 -0400

    parallel ingest

--- ingest.py
+++ ingest.py
@@ -5,23 +5,31 @@ import requests
 import logging
 from tx.fhir.utils import bundle, unbundle
 from tx.logging.utils import getLogger
+from joblib import Parallel, delayed
+from functools import partial
 
-logger = getLogger(__name__, logging.INFO)
+threads, base_url, input_dir = sys.argv[1:]
 
-base_url, input_dir = sys.argv[1:]
+num_threads = int(threads)
 
-for root, _, files in os.walk(input_dir, followlinks=True):
-    for file in files:
-        path = f"{root}/{file}"
-        with open(path) as input_stream:
-            logger.info(f"loading {path}")
-            obj = json.load(input_stream)
-            rescs = unbundle(obj).value
-            nrescs = len(rescs)
-            logger.info(f"{nrescs} resources loaded")
-            maxlen = 1024
-            for i in range(0, nrescs, maxlen):
-                subrescs = rescs[i: min(i+maxlen, nrescs)]
-                subobj = bundle(subrescs)
-                logger.info(f"ingesting {path} {i}")
-                requests.post(f"{base_url}/Bundle", json=subobj)
+paths = [f"{root}/{file}" for root, _, files in os.walk(input_dir, followlinks=True) for file in files]
+
+
+
+def handle_path(path):
+    logger = getLogger(f"{__name__}{os.getpid()}", logging.INFO)
+
+    with open(path) as input_stream:
+        logger.info(f"loading {path}")
+        obj = json.load(input_stream)
+        rescs = unbundle(obj).value
+        nrescs = len(rescs)
+        logger.info(f"{nrescs} resources loaded")
+        maxlen = 1024
+        for i in range(0, nrescs, maxlen):
+            subrescs = rescs[i: min(i+maxlen, nrescs)]
+            subobj = bundle(subrescs)
+            logger.info(f"ingesting {path} {i}")
+            requests.post(f"{base_url}/Bundle", json=subobj)
+
+Parallel(n_jobs=num_threads)(delayed(handle_path)(path) for path in paths)
