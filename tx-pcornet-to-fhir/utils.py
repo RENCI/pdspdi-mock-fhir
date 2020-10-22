6e2252d312a5986c632a7ff151b1cabc2827f119
hyi <hongyi@renci.org>
2020-10-21 21:43:29 -0400

    intermediate work trying to get pcori data ingestion added to fhir plugin

---
+++ tx-pcornet-to-fhir/utils.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import os
+import json
+
+
+def bundle(entry=None):
+    fhir_bundle = {
+        "resourceType": "Bundle",
+        "type": "collection"
+    }
+    if entry:
+        fhir_bundle["entry"] = entry
+    return fhir_bundle
+
+
+def write_fhir_json(content, input_path, input_file_name):
+    if not os.path.exists(input_path):
+        os.makedirs(input_path)
+    with open(os.path.join(input_path, input_file_name), 'w') as outfile:
+        json.dump(content, outfile, indent=4)
+
+
+def get_input_df(table_cd, input_path, map_df, use_cols=None):
+    input_file_name = f'{table_cd}.csv'
+    input_file = os.path.join(input_path, input_file_name)
+    subset_map_df = map_df.loc[table_cd, :]
+
+    if use_cols:
+        input_df = pd.read_csv(input_file, sep='|', usecols=use_cols)
+    else:
+        input_df = pd.read_csv(input_file, sep='|')
+    return input_df, subset_map_df
